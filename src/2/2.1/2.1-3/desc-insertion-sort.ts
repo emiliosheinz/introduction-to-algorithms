1d27d0ff12895b4ecdf591544aa7e1f27a88ebfa
Emilio Schaedler Heinzmann <emiliosheinz@gmail.com>
2023-10-14 22:34:10 -0300

    docs: :bulb: removes commented python code

--- src/2/2.1/2.1-3/desc-insertion-sort.ts
+++ src/2/2.1/2.1-3/desc-insertion-sort.ts
@@ -1,12 +1,3 @@
-// def desc_insertion_sort(a, n):
-//     for i in range(1, n):
-//         key = a[i]
-//         j = i - 1
-//         while j >= 0 and a[j] < key:
-//             a[j + 1] = a[j]
-//             j = j - 1
-//         a[j + 1] = key
-
 export function descInsertionSort(a: Array<number>, n: number) {
   for (let i = 1; i < n; i++) {
     const key = a[i]
